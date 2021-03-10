145ef88a387c71d36b36932c3d9c4a19d4848cd4
Julian Simonetti <guerrier10@live.fr>
2021-03-10 16:30:14 +0100

    Essai de fix ajax via GH

--- js/pageSwitch.js
+++ js/pageSwitch.js
@@ -1,6 +1,6 @@
 function loadPage(id) {
     $.ajax({
-        url: "/pages/"+id+".html",
+        url: "./pages/"+id+".html",
         dataType: "html",
         success: function (page) {
             $("#content").fadeOut(200).html(page).fadeIn(200);
